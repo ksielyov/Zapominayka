9157cb89b0716bd863872185e57030ddd714d312
Mikhail Kiselyov <mihailkiseliow@yandex.ru>
2022-06-20 18:53:57 +0500

    Button UI should call onPress function

--- src/shared/ui/button/button.test.tsx
+++ src/shared/ui/button/button.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import {Button} from './index';
+import {fireEvent, render} from '@testing-library/react-native';
 
 it('button should render correctly', () => {
   const tree = renderer
@@ -12,4 +13,16 @@ it('button should render correctly', () => {
     )
     .toJSON();
   expect(tree).toMatchSnapshot();
+});
+
+it('should call onPress function', () => {
+  const buttonTitle = 'Button test';
+  const onPressEvent = jest.fn();
+
+  const {getByText} = render(
+    <Button title={buttonTitle} onPress={onPressEvent} />,
+  );
+
+  fireEvent.press(getByText(buttonTitle));
+  expect(onPressEvent).toHaveBeenCalled();
 });
